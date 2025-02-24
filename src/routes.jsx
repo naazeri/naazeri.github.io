26fa27d70dd2fef88e7e6cdf3acc5676a0d9172f
Reza Nazeri <r.nazeri.vitrin@gmail.com>
2025-02-25 00:21:38 +0330

    use HashRouter instead of BrowserRouter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BlogPostPage from './pages/BlogPostPage';
 import BlogPostsPage from './pages/BlogPostsPage';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPostsPage />} />
         <Route path="/blog/:slug" element={<BlogPostPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
