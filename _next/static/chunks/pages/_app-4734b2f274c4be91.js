c5260309adc66327147ca558563778ee8fe610d8
Reza Nazeri <reza.armani75@gmail.com>
2026-09-19 02:33:16 +0330

    Updates

---
+++ _next/static/chunks/pages/_app-4734b2f274c4be91.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{9827:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(5858)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(9827),n(4364))),_N_E=_.O()}]);
